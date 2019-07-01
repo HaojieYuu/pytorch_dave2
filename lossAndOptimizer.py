414e642da993818190cf3da2992f79261a72641e
hyu <yuhaojie_2008@126.com>
2019-07-01 16:41:14 +0200

    delete lossAndOptimizer

--- lossAndOptimizer.py
+++
@@ -1,7 +0,0 @@
-import torch.optim as optim
-import torch.nn as nn
-
-def createLossAndOptimizer(net, learning_rate = 0.001, weight_decay = 0.001):
-	criterion = nn.MSELoss()
-	optimizer = optim.Adam(net.parameters(), lr = learning_rate, weight_decay = weight_decay)
-	return criterion, optimizer
